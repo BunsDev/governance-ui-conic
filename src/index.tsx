c6523813eaecd8bb277252296a6aa46a368f024d
C3PO <c3p0-skywalker@protonmail.com>
2023-07-03 15:14:45 +0100

    :rotating_light: fix lint error

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import "./polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
